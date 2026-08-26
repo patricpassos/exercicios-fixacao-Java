df1788515224c3f29a1a7986de45d602a927d7ca
patricpassos <patrickpassos93@gmail.com>
2026-08-26 15:09:29 -0300

    Exercicios de plataformas

---
+++ dev_superior/estrutura_repetitiva/ProblemaDentroForaFor.java
@@ -0,0 +1,34 @@
+package estrutura_repetitiva;
+import java.util.Scanner;
+
+public class ProblemaDentroForaFor {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantos nueros você vai digitar? ");
+		int entrada = sc.nextInt();
+
+		int contDentro = 0;
+		int contFora = 0;
+		for (int i = 1; i <= entrada; i++) {
+			System.out.print("Digite um numero: ");
+			int x = sc.nextInt();
+
+			if (x >= 10 && x <= 20) {
+				contDentro++;
+			} else {
+				contFora++;
+			}
+
+		}
+
+		System.out.println(contDentro + " DENTRO");
+		System.out.println(contFora + " FORA");
+
+		sc.close();
+
+	}
+
+}
---
+++ dev_superior/estrutura_sequencial/ProblemaTroco.java
@@ -0,0 +1,29 @@
+package estrutura_sequencial;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaTroco {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Preço do produto: ");
+		double preco = sc.nextDouble();
+		
+		System.out.print("Quantidade comprada: ");
+		int quantidade = sc.nextInt();
+		
+		System.out.print("Dinheiro recebido: ");
+		double dinheiro = sc.nextDouble();
+		
+		double troco = dinheiro - (preco * quantidade);
+		
+		System.out.print("TROCO = " + troco);
+		
+		sc.close();
+
+	}
+
+}
